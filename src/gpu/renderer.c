32dfa2119b585ebabeb1317d76bbcbdf81f95a08
hibiki <20015683+hibikidesu@users.noreply.github.com>
2020-12-31 17:50:18 +0800

    OWO

--- src/gpu/renderer.c
+++ src/gpu/renderer.c
@@ -7,41 +7,40 @@
 #include "renderer.h"
 #include "../utils/logger.h"
 
-static SDL_Window *g_Window = NULL;
-
-// void interpolate(RendererColor *color1, RendererColor *color2, float fraction) {
-// 	uint8_t r = (uint8_t)((color2->r - color1->r) * fraction + color1->r);
-// 	uint8_t g = (uint8_t)((color2->g - color1->g) * fraction + color1->g);
-// 	uint8_t b = (uint8_t)((color2->b - color1->b) * fraction + color1->b);
-// 	SDL_SetRenderDrawColor(g_Renderer, r, g, b, 255);
-// }
-
-// void algoDDA(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2) {
-// 	float dx = (x2 - x1);
-// 	float dy = (y2 - y1);
-// 	float step;
-// 	float x, y;
-// 	int i;
-
-// 	if (abs(dx) >= abs(dy)) {
-// 		step = abs(dx);
-// 	} else {
-// 		step = abs(dy);
-// 	}
-
-// 	dx = (dx / step);
-// 	dy = (dy / step);
-// 	x = x1;
-// 	y = y1;
-// 	for (i = 0; i < step; i++) {
-// 		SDL_RenderDrawPoint(g_Renderer, (int)x, (int)y);
-// 		x += dx;
-// 		y += dy;
-// 	}
-// }
+static GPU_Target *g_Screen = NULL;
 
 void renderer_DrawTriangleShade(struct RendererPosition *positions, struct RendererColor *colors) {
-
+	// vertices * (x, y + r, g, b, a)
+	float values[3 * (2 + 4)] = {
+		// 1st Position
+		// X, Y
+		(float)positions[0].x,
+		(float)positions[0].y,
+		// RGBA
+		(float)colors[0].r / 255,
+		(float)colors[0].g / 255,
+		(float)colors[0].b / 255,
+		1.0,
+		// 2nd Position
+		// X, Y
+		(float)positions[1].x,
+		(float)positions[1].y,
+		// RGBA
+		(float)colors[1].r / 255,
+		(float)colors[1].g / 255,
+		(float)colors[1].b / 255,
+		1.0,
+		// 3rd Position
+		// X, Y
+		(float)positions[2].x,
+		(float)positions[2].y,
+		// RGBA
+		(float)colors[2].r / 255,
+		(float)colors[2].g / 255,
+		(float)colors[2].b / 255,
+		1.0,
+	};
+	GPU_TriangleBatch(NULL, g_Screen, 3, values, 0, NULL, GPU_BATCH_XY_RGBA);
 }
 
 struct RendererColor renderer_GetColorFromGP0(uint32_t value) {
@@ -60,29 +59,22 @@ struct RendererPosition renderer_GetPositionFromGP0(uint32_t value) {
 }
 
 void renderer_Update() {
-	
+	GPU_Flip(g_Screen);
+	GPU_Clear(g_Screen);
 }
 
 void renderer_Init() {
-	// Init SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		log_Error("Failed to init SDL2: %s", SDL_GetError());
+	// Init SDL_gpu
+	g_Screen = GPU_Init(1024, 512, GPU_DEFAULT_INIT_FLAGS);
+	if (g_Screen == NULL) {
+		log_Error("Failed to init sdl-gpu: %s", SDL_GetError());
 		exit(1);
 	}
-
-	g_Window = SDL_CreateWindow("uwu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 512, SDL_WINDOW_OPENGL);
-	if (g_Window == NULL) {
-		log_Error("Failed to create window: %s", SDL_GetError());
-		exit(1);
-	}
-
-	// Wait for it to be ready
-	SDL_Delay(80);
 
 	renderer_Update();
 }
 
 void renderer_Destroy() {
-	SDL_DestroyWindow(g_Window);
+	GPU_Quit();
 	SDL_Quit();
 }
